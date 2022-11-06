3d9dfaa96ae46dacc7aee3d0264d112613d61511
Jonathan Martins <dejimartins99@gmail.com>
2022-11-06 09:18:44 -0800

    styled home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import { signInWithRedirect, auth, provider } from "../config/index";
+import { FiChevronRight } from "react-icons/fi";
+import "../styles/home.css";
 
 const Home = () => {
   const signIn = (event) => {
@@ -7,9 +9,19 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <p>Welcome</p>
-      <button onClick={(event) => signIn(event)}>Sign in</button>
+    <div className="home">
+      <div className="logo">
+        <p>" My Github Repo "</p>
+      </div>
+      <div className="welcome-back">
+        <h3>Hello, Welcome Back</h3>
+        <p>A preview of your github repositories</p>
+        <div className="sign-in-button">
+          <button onClick={(event) => signIn(event)}>
+            <FiChevronRight size={25} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
